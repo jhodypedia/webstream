bc31d54869e6e69786be9f56950cf143fcd5f25b
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-05 21:54:45 +0700

    Update VideoVariant.js

--- models/VideoVariant.js
+++ models/VideoVariant.js
@@ -1,10 +1,13 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from './index.js';
+import { sequelize } from './db.js';
 
 export const VideoVariant = sequelize.define('VideoVariant', {
-  id: { type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   video_id: DataTypes.STRING,
   label: DataTypes.STRING,
   bandwidth: DataTypes.INTEGER,
-  hls_url: DataTypes.TEXT
+  hls_url: DataTypes.TEXT,
+}, {
+  tableName: 'video_variants',
+  timestamps: true,
 });
