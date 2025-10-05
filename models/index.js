02018570c2ca37983cdc252a4d421e7ed3e154c7
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-06 01:01:27 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
+// 📁 models/index.js
 import { sequelize } from './db.js';
 import { User } from './User.js';
 import { Video } from './Video.js';
 import { VideoVariant } from './VideoVariant.js';
 import { Job } from './Job.js';
+import { Setting } from './Setting.js';
 
 // 💡 Hubungan antar model
 Video.hasMany(VideoVariant, { foreignKey: 'video_id', onDelete: 'CASCADE' });
@@ -11,5 +13,16 @@ VideoVariant.belongsTo(Video, { foreignKey: 'video_id' });
 Video.hasMany(Job, { foreignKey: 'video_id', onDelete: 'CASCADE' });
 Job.belongsTo(Video, { foreignKey: 'video_id' });
 
-// Export semua instance
-export { sequelize, User, Video, VideoVariant, Job };
+// 🧩 Hubungan video dan user (owner)
+User.hasMany(Video, { foreignKey: 'owner_user_id', as: 'videos' });
+Video.belongsTo(User, { foreignKey: 'owner_user_id', as: 'owner' });
+
+// 🔧 Setting global (tidak punya relasi, 1 table key-value)
+export {
+  sequelize,
+  User,
+  Video,
+  VideoVariant,
+  Job,
+  Setting
+};
