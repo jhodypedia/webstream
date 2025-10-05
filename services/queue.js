7cb9a5e740b95ae5376f52c760a4dc683a345270
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-05 21:57:25 +0700

    Update queue.js

--- services/queue.js
+++ services/queue.js
@@ -1,6 +1,8 @@
 import { Queue } from 'bullmq';
-import dotenv from 'dotenv'; dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const transcodeQueue = new Queue('transcode:video', {
+// ✅ Gunakan nama queue tanpa karakter “:”
+export const transcodeQueue = new Queue('transcode_video', {
   connection: { url: process.env.REDIS_URL }
 });
