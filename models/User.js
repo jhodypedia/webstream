85b1351d7560bdfa857f2dbe863f1de7d4b5156b
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-05 21:54:06 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from './index.js';
+import { sequelize } from './db.js';
 
 export const User = sequelize.define('User', {
-  id: { type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
-  email: DataTypes.STRING,
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  email: { type: DataTypes.STRING, unique: true },
   password_hash: DataTypes.STRING,
   fullname: DataTypes.STRING,
-  telegram_user_id: DataTypes.STRING
+  telegram_user_id: DataTypes.STRING,
+}, {
+  tableName: 'users',
+  timestamps: true,
 });
